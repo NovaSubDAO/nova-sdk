79d0299300622a6d1d662d7fe7ee4a2f8c5fd1cc
lakonema2000@gmail <lakonema2000@gmail.com>
2024-04-23 07:42:07 +0200

    add support for sdai ethereum

---
+++ nova_sdk/config.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+
+from exceptions import Web3ShouldBeConnected
+from utils import get_env
+from web3 import Web3
+
+SDAI_ADDRESSES_MAP = {"ETHEREUM": "0x83F20F44975D03b1b09e64809B757c47f942BEeA"}
+
+
+@dataclass
+class Config:
+    private_key: str
+    rpc_endpoint: str
+    chain_id: str
+    sdai_address: str
+
+
+def load_config():
+    config = Config(
+        private_key=get_env("PRIVATE_KEY"),
+        rpc_endpoint=get_env("RPC_ENDPOINT"),
+        chain_id=get_env("CHAIN_ID"),
+        sdai_address=SDAI_ADDRESSES_MAP[get_env("NETWORK")],
+    )
+    return config
+
+
+def load_config_and_web3():
+    config = load_config()
+
+    w3 = Web3(Web3.HTTPProvider(config.rpc_endpoint))
+    if not w3.is_connected():
+        raise ValueError(Web3ShouldBeConnected)
+
+    config.sdai_address = w3.to_checksum_address(config.sdai_address)
+
+    account = w3.eth.account.from_key(config.private_key)
+    w3.eth.default_account = account.address
+
+    return config, w3
---
+++ nova_sdk/withdraw.py
@@ -0,0 +1,41 @@
+import eth_abi
+from config import load_config_and_web3
+from web3.exceptions import ContractLogicError
+
+config, w3 = load_config_and_web3()
+
+
+def withdraw(assets_, receiver_, owner_):
+    function_signature = "withdraw(uint256,address,address)"
+    function_selector = w3.keccak(text=function_signature)[:4]
+
+    receiver = w3.to_checksum_address(receiver_)
+    owner = w3.to_checksum_address(owner_)
+
+    encoded_arguments = eth_abi.encode(["uint256", "address", "address"], [assets_, receiver, owner])
+
+    data = function_selector + encoded_arguments
+
+    try:
+        gas_estimate = w3.eth.estimate_gas(
+            {"from": w3.eth.default_account, "to": config.sdai_address, "value": 0, "data": data}
+        )
+    except ContractLogicError as e:
+        print(f"Gas estimation failed due to contract logic error: {e}")
+        gas_estimate = 20_00_000  # fallback gas limit
+
+    transaction = {
+        "to": config.sdai_address,
+        "value": 0,
+        "gasPrice": w3.eth.gas_price,
+        "gas": gas_estimate,
+        "nonce": w3.eth.get_transaction_count(w3.eth.default_account),
+        "data": data,
+        "chainId": config.chain_id,
+    }
+
+    signed_txn = w3.eth.account.sign_transaction(transaction, config.private_key)
+    tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    print(tx_receipt)

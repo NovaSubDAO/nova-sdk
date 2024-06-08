c85632dbc5647ad01c64398d8c2ce34d9b6b0570
lakonema2000 <lakonema2000@gmail.com>
2024-06-08 14:04:37 +0200

    added create deposit and withdraw txs

--- go/main.go
+++ go/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+    "math/big"
     "log"
 
+	"github.com/ethereum/go-ethereum/common"
     "github.com/NovaSubDAO/nova-sdk/go/pkg/sdk"
 )
 
 func main() {
     // check vault address on ethereum
+    log.Println("Nova SDK: Ethereum")
     chainId := int64(1)
     novaSdk, err := sdk.NewNovaSDK("", chainId)
     if err != nil {
@@ -15,8 +18,23 @@ func main() {
     }
     log.Println("chain id:", novaSdk.Config.ChainId)
     log.Println("vault address:", novaSdk.Config.VaultAddress)
+    amount := big.NewInt(100)
+    referral := big.NewInt(123)
+    tx, err := novaSdk.SdkDomain.CreateDepositTransaction(common.HexToAddress("0x83F20F44975D03b1b09e64809B757c47f942BEeA"), amount, referral)
+    if err != nil {
+        log.Fatal("Error creating deposit transaction:", err)
+    }
+    log.Println("deposit tx:", tx)
+    tx, err = novaSdk.SdkDomain.CreateWithdrawTransaction(common.HexToAddress("0x83F20F44975D03b1b09e64809B757c47f942BEeA"), amount, referral)
+    if err != nil {
+        log.Fatal("Error creating withdraw transaction:", err)
+    }
+    log.Println("withdraw tx:", tx)
+    log.Println(" ")
+
 
     // check vault address on optimism
+    log.Println("Nova SDK: Optimism")
     chainId = 10
     novaSdk, err = sdk.NewNovaSDK("", chainId)
     if err != nil {
@@ -24,4 +42,14 @@ func main() {
     }
     log.Println("chain id:", novaSdk.Config.ChainId)
     log.Println("vault address:", novaSdk.Config.VaultAddress)
+    tx, err = novaSdk.SdkDomain.CreateDepositTransaction(common.HexToAddress("0x83F20F44975D03b1b09e64809B757c47f942BEeA"), amount, referral)
+    if err != nil {
+        log.Fatal("Error creating deposit transaction:", err)
+    }
+    log.Println("deposit tx:", tx)
+    tx, err = novaSdk.SdkDomain.CreateWithdrawTransaction(common.HexToAddress("0x83F20F44975D03b1b09e64809B757c47f942BEeA"), amount, referral)
+    if err != nil {
+        log.Fatal("Error creating withdraw transaction:", err)
+    }
+    log.Println("withdraw tx:", tx)
 }
--- go/pkg/sdk/ethereum/sdk_ethereum.go
+++ go/pkg/sdk/ethereum/sdk_ethereum.go
@@ -1,12 +1,19 @@
 package ethereum
 
 import (
-    "math/big"
+    "context"
+	"encoding/json"
     "fmt"
+    "log"
+    "math/big"
+    "os"
+    "strings"
 
-    "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-    "github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/ethclient"
     "github.com/NovaSubDAO/nova-sdk/go/pkg/config"
     "github.com/NovaSubDAO/nova-sdk/go/pkg/contracts"
 )
@@ -81,12 +88,108 @@ func (sdk *SdkEthereum) GetSlippage(amount *big.Int) (*big.Int, error) {
 	return nil, fmt.Errorf("Not yet implemented")
 }
 
-func (sdk *SdkEthereum) CreateDepositTransaction(big.Int, big.Int) (string, error) {
-	return "", fmt.Errorf("Not yet implemented")
+func (sdk *SdkEthereum) CreateDepositTransaction(fromAddress common.Address, amount *big.Int, referral *big.Int) (string, error) {
+	client, err := ethclient.Dial(sdk.Config.RpcEndpoint)
+	if err != nil {
+		return "", fmt.Errorf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return "", fmt.Errorf("Failed to get nonce: %v", err)
+	}
+
+    gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return "", fmt.Errorf("Failed to suggest gas price: %v", err)
+	}
+
+    abiPath := "pkg/sdk/ethereum/abis/SavingsDai.json"
+    file, err := os.ReadFile(abiPath)
+    if err != nil {
+		return "", fmt.Errorf("Failed to read ABI file: %w", err)
+    }
+
+    parsedABI, err := abi.JSON(strings.NewReader(string(file)))
+    if err != nil {
+		return "", fmt.Errorf("Failed to parse ABI: %w", err)
+    }
+
+    contractAddress := common.HexToAddress(sdk.Config.VaultAddress)
+
+	data, err := parsedABI.Pack("deposit", amount, fromAddress)
+	if err != nil {
+		return "", fmt.Errorf("ABI pack failed: %v", err)
+	}
+
+	// Estimating the gas needed for the transaction
+	msg := ethereum.CallMsg{From: fromAddress, To: &contractAddress, GasPrice: gasPrice, Value: big.NewInt(0), Data: data}
+	gasLimit, err := client.EstimateGas(context.Background(), msg)
+	if err != nil {
+		log.Printf("Gas estimation failed, using fallback gas limit: %v", err)
+		gasLimit = 2000000 // Fallback gas limit
+	}
+
+    tx := types.NewTransaction(nonce, contractAddress, big.NewInt(0), gasLimit, gasPrice, data)
+
+	txJSON, err := json.Marshal(tx)
+	if err != nil {
+		return "", fmt.Errorf("Failed to marshal transaction: %w", err)
+	}
+
+    return string(txJSON), nil
 }
 
-func (sdk *SdkEthereum) CreateWithdrawTransaction(big.Int, big.Int) (string, error) {
-	return "", fmt.Errorf("Not yet implemented")
+func (sdk *SdkEthereum) CreateWithdrawTransaction(fromAddress common.Address, amount *big.Int, referral *big.Int) (string, error) {
+	client, err := ethclient.Dial(sdk.Config.RpcEndpoint)
+	if err != nil {
+		return "", fmt.Errorf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return "", fmt.Errorf("Failed to get nonce: %v", err)
+	}
+
+    gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return "", fmt.Errorf("Failed to suggest gas price: %v", err)
+	}
+
+    abiPath := "pkg/sdk/ethereum/abis/SavingsDai.json"
+    file, err := os.ReadFile(abiPath)
+    if err != nil {
+		return "", fmt.Errorf("Failed to read ABI file: %w", err)
+    }
+
+    parsedABI, err := abi.JSON(strings.NewReader(string(file)))
+    if err != nil {
+		return "", fmt.Errorf("Failed to parse ABI: %w", err)
+    }
+
+    contractAddress := common.HexToAddress(sdk.Config.VaultAddress)
+
+	data, err := parsedABI.Pack("withdraw", amount, fromAddress, fromAddress)
+	if err != nil {
+		return "", fmt.Errorf("ABI pack failed: %v", err)
+	}
+
+	// Estimating the gas needed for the transaction
+	msg := ethereum.CallMsg{From: fromAddress, To: &contractAddress, GasPrice: gasPrice, Value: big.NewInt(0), Data: data}
+	gasLimit, err := client.EstimateGas(context.Background(), msg)
+	if err != nil {
+		log.Printf("Gas estimation failed, using fallback gas limit: %v", err)
+		gasLimit = 2000000 // Fallback gas limit
+	}
+
+    tx := types.NewTransaction(nonce, contractAddress, big.NewInt(0), gasLimit, gasPrice, data)
+
+	txJSON, err := json.Marshal(tx)
+	if err != nil {
+		return "", fmt.Errorf("Failed to marshal transaction: %w", err)
+	}
+
+    return string(txJSON), nil
 }
 
 func (sdk *SdkEthereum) Deposit(assets *big.Int, receiver common.Address, referral big.Int) (*types.Transaction, error) {

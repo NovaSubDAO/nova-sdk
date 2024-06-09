55d5c35e37121ad5ca67302a9c916bf3dbeaf43b
lakonema2000 <lakonema2000@gmail.com>
2024-06-09 10:48:57 +0200

    formatted go files with gofmt

--- go/pkg/config/config.go
+++ go/pkg/config/config.go
@@ -1,35 +1,35 @@
 package config
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type Config struct {
-    VaultAddress  string `json:"vaultAddress"`
-    VaultDecimals  int64  `json:"vaultDecimals"`
-    RpcEndpoint   string
-    ChainId       int64
+	VaultAddress  string `json:"vaultAddress"`
+	VaultDecimals int64  `json:"vaultDecimals"`
+	RpcEndpoint   string
+	ChainId       int64
 }
 
 func LoadConfig(rpcEndpoint string, chainId int64) (*Config, error) {
-    var config Config
-    switch chainId {
-    case 1:
-        config = Config{
-            RpcEndpoint:   rpcEndpoint,
-            ChainId:       chainId,
-            VaultAddress:  "0x83F20F44975D03b1b09e64809B757c47f942BEeA",
-            VaultDecimals: 18,
-        }
-    case 10:
-        config = Config{
-            RpcEndpoint:   rpcEndpoint,
-            ChainId:       chainId,
-            VaultAddress:  "0x36A2f7Fb07c102415afe2461a9A43377970E081c",
-            VaultDecimals: 18,
-        }
-    default:
-        return nil, fmt.Errorf("unsupported chain id %d", chainId)
-    }
-    return &config, nil
+	var config Config
+	switch chainId {
+	case 1:
+		config = Config{
+			RpcEndpoint:   rpcEndpoint,
+			ChainId:       chainId,
+			VaultAddress:  "0x83F20F44975D03b1b09e64809B757c47f942BEeA",
+			VaultDecimals: 18,
+		}
+	case 10:
+		config = Config{
+			RpcEndpoint:   rpcEndpoint,
+			ChainId:       chainId,
+			VaultAddress:  "0x36A2f7Fb07c102415afe2461a9A43377970E081c",
+			VaultDecimals: 18,
+		}
+	default:
+		return nil, fmt.Errorf("unsupported chain id %d", chainId)
+	}
+	return &config, nil
 }
